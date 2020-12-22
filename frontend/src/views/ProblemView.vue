f20f55e3d2bfd04519b7bb522082b8280dd6a4df
Darya Pakhomova <mrrlanie@gmail.com>
2020-12-23 01:29:55 +1000

    funcs for buttons, colors + reject/accept + disabled when complited

--- frontend/src/views/ProblemView.vue
+++ frontend/src/views/ProblemView.vue
@@ -4,21 +4,29 @@
       <div class="bx--col-lg-10">
         <br>
         <h3> Задание {{ problem.id }}. {{ problem.name }}
-          <cv-button kind="secondary" size="small">Accept</cv-button>
-          <cv-button kind="danger" size="small">Reject</cv-button> </h3>
-
-        <!-- TODO: "Accept" button color: grey -> green -->
+          <cv-button class="rejected" kind="danger" size="small" v-on:click="rejectHandler">Rejected</cv-button> /
+          <cv-button class="accepted" kind="secondary" size="small" v-on:click="acceptHandler">Accepted</cv-button>
+        </h3>
 
         <h4> Описание: {{ problem.description }} </h4>
         <br>
         <label>
           <textarea
+            v-if="problem.completed == false"
             v-model="areaData"
             id="text"
             cols="130"
             placeHolder="Сюда кодить надо"
             rows="20">
           </textarea>
+          <textarea
+            v-else
+            cols="130"
+            rows="20">
+          </textarea>
+
+          <!-- TODO: if complited -> textarea.value = last submit text -->
+
         </label>
         <div>
           <br>
@@ -30,7 +38,8 @@
             <cv-dropdown-item value="Brainfuck">Brainfuck</cv-dropdown-item>
           </cv-dropdown>
           <br>
-          <cv-button v-on:click="buttonHandler"> Submit! </cv-button>
+          <cv-button v-on:click="buttonHandler" v-if="problem.completed == false"> Submit! </cv-button>
+          <cv-button v-on:click="buttonHandler" v-else disabled="true"> Submit! </cv-button>
         </div>
       </div>
       <div class="bx--col-lg-3">
@@ -38,7 +47,8 @@
         <ul>
           <li v-for="sub in submits" :key="sub.id">
             <cv-tile light>
-              <cv-button size="small" kind="ghost"> <h5> Sub #{{sub.id}} </h5> </cv-button>
+              <cv-button size="small" kind="ghost" v-if="problem.completed == false" v-on:click="submitHandler(sub.id)"> <h5> Sub #{{sub.id}} </h5> </cv-button>
+              <cv-button size="small" kind="ghost" v-else disabled="true" v-on:click="submitHandler(sub.id)"> <h5> Sub #{{sub.id}} </h5> </cv-button>
             </cv-tile>
           </li>
         </ul>
@@ -50,15 +60,13 @@
         <cv-tile light>
           <cv-button size="small" kind="ghost"> <h5> Student: {{studentsArray[Math.floor(Math.random() * 2)].name}} </h5> </cv-button>
         </cv-tile>
-          <!-- TODO: students submit realisation -->
+          <!-- TODO: normal student name realisation -->
         </li>
         </ul>
       </div>
     </div>
   </div>
 </template>
-
-<!-- TODO: links like: press tag -> label.text = text of submit -->
 
 <script lang="ts">
 
@@ -79,6 +87,18 @@ export default class ProblemView extends Vue {
   get problem() {
     return this.problemsArray[0];
   }
+
+  submitHandler(idSub: number) {
+    const submit = this.submits[idSub - 1];
+    this.areaData = Object.values(submit)[1];
+    console.log(Object.values(submit)[1])
+  }
+  acceptHandler (){
+    this.problem.completed = true;
+  }
+  rejectHandler (){
+    this.problem.completed = false;
+  }
   buttonHandler() {
     if (this.areaData !== ''){
       this.submits.push({
@@ -86,6 +106,7 @@ export default class ProblemView extends Vue {
         title: this.areaData,
       })
       this.submitCounter++;
+      console.log(this.submits)
     } else {
      alert('Type some code!')
     }
@@ -96,6 +117,13 @@ export default class ProblemView extends Vue {
 </script>
 <!--    TODO: solve a problem w/ getting single problem from array -->
 
-<style scoped>
-
+<style scoped lang="stylus">
+.accepted{
+  background: #2ea92e;
+  border-radius: 25px;
+}
+.rejected{
+  background: #cd1d1d;
+  border-radius: 25px;
+}
 </style>
