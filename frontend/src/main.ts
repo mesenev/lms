024e9e2613a0250005a9f093f614a06f423a12db
Maingart <maingartvlad@outlook.com>
2020-12-28 01:25:11 +1000

    Fix main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,13 +5,7 @@ import { store } from '@/store';
 import CarbonComponentsVue from '@carbon/vue/src/index';
 import Vue from 'vue';
 
-import { CarbonIconsVue } from '@carbon/icons-vue';
-
 Vue.use(CarbonComponentsVue);
-// TODO долго собирается/перезагружается страница
-Vue.use(CarbonIconsVue, {
-  components: {},
-});
 
 new Vue({
   router,
