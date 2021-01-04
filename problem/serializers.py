44b338971ac5ff85d332a7274c02b87b94d4d119
Main <maingartvlad@outlook.com>
2021-01-05 05:21:09 +1000

    Temporary Endpoint Create

--- problem/serializers.py
+++ problem/serializers.py
@@ -1,20 +1,33 @@
 from rest_framework import serializers
 
 from problem.models import Problem, Submit
+from users.serializers import DefaultUserSerializer
 
 
 class SubmitSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
+    problem = serializers.PrimaryKeyRelatedField(read_only=True)
+    student = DefaultUserSerializer(read_only=True)
+    content = serializers.CharField()
+    status = serializers.CharField()
 
     def update(self, instance, validated_data):
         pass
 
     def create(self, validated_data):
-        pass
+        request = self.context.get('request')
+        student = request.user if request and hasattr(request, 'user') else None
+        problem = request.problem if request and hasattr(request, 'problem') else (
+            Problem.objects.last()
+        )
+        return Submit.objects.create(**validated_data, **{
+            'student': student,
+            'problem': problem
+        })
 
     class Meta:
         model = Submit
-        fields = '__all__'
+        fields = ('id', 'problem', 'student', 'content', 'status')
 
 
 class ProblemSerializer(serializers.Serializer):
