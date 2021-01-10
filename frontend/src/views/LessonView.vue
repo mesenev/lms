190079413b0e7eb96bbfc47d0c63d87db4f00488
mesenev <heredemonsnearby@mail.ru>
2021-01-10 16:31:40 +1000

    some loaders

--- frontend/src/views/LessonView.vue
+++ frontend/src/views/LessonView.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="bx--grid">
-    <div v-if="!loading" class="bx--row">
+    <div class="bx--row">
       <div class="bx--col-lg-16">
         <cv-tile kind="standard">
-          <h1>{{ lesson.name }}</h1>
-          <p>{{ lesson.deadline }}</p>
+          <h1 v-if="!loading">{{ lesson.name }}</h1>
+          <cv-skeleton-text v-else :heading="true" width="'50%'"/>
+          <p v-if="!loading">{{ lesson.deadline }}</p>
+          <cv-skeleton-text v-else width="'35%'"/>
         </cv-tile>
         <h4> Задачи урока: </h4>
       </div>
@@ -15,37 +17,43 @@
       <div class="less bx--col-lg-9">
         <div class="classwork">
           <h4>Классная работа</h4>
-          <cv-accordion v-for="less in classwork" :key="less.id">
-            <Problem :problem-prop='less'/>
-          </cv-accordion>
+          <div v-if="!loading">
+            <cv-accordion v-for="less in classwork" :key="less.id">
+              <Problem :problem-prop='less'/>
+            </cv-accordion>
+          </div>
+          <div v-else>
+            <cv-accordion-skeleton/>
+          </div>
         </div>
         <br>
         <br>
         <div class="homework">
+
           <h4>Домашнаяя работа</h4>
-          <cv-accordion v-for="less in homework" :key="less.id">
-            <Problem :problem-prop='less'/>
-          </cv-accordion>
+          <div v-if="!loading">
+            <cv-accordion v-for="less in homework" :key="less.id">
+              <Problem :problem-prop='less'/>
+            </cv-accordion>
+          </div>
+          <div v-else>
+            <cv-accordion-skeleton/>
+          </div>
         </div>
       </div>
       <div class="bx--col-lg-1">
       </div>
       <div class="bx--col-lg-6">
         <div class="less bx--row-lg-6">
-          <h4>Пользователи:</h4>
-        </div>
-        <div class="bx--row-lg-6">
-          <br>
-        </div>
-        <div class="less bx--row-lg-6">
           <h4>Материалы</h4>
-          <cv-structured-list selectable>
+          <cv-structured-list v-if="!loading" selectable>
             <template slot="items">
               <cv-structured-list-item v-for="material in materials" :key="material.id">
                 <Material :material-prop='material'/>
               </cv-structured-list-item>
             </template>
           </cv-structured-list>
+          <cv-data-table-skeleton v-else :columns="1"/>
         </div>
       </div>
     </div>
@@ -61,7 +69,7 @@ import ProblemModel from '@/models/ProblemModel';
 import { lessonStore } from '@/store';
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
-@Component({ components: {Material, Problem } })
+@Component({ components: { Material, Problem } })
 export default class LessonView extends Vue {
   @Prop() lessonId!: number;
   store = lessonStore;
