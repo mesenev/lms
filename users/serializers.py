3a92f42bd041c5d8a886e6b8aa22d2ab4d34f1d4
mesenev <heredemonsnearby@mail.ru>
2021-01-11 15:31:59 +1000

    staff_for

--- users/serializers.py
+++ users/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
 
-from users.models import User
+from users.models import User, CourseAssign
 
 
 class DefaultUserSerializer(serializers.ModelSerializer):
+    staff_for = serializers.SerializerMethodField()
+
+    def get_staff_for(self, obj):
+        return list(CourseAssign.objects.filter(is_staff=True, user=obj).values_list('id', flat=True))
+
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'id']
-
-
+        fields = ['username', 'first_name', 'last_name', 'id', 'staff_for']
