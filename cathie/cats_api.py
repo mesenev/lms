ff012ab1df8469da0a3f9de9583a4fc1b733b27d
Main <maingartvlad@outlook.com>
2021-01-18 19:20:40 +1000

    save changes

--- cathie/cats_api.py
+++ cathie/cats_api.py
@@ -1,4 +1,5 @@
 import json
+from random import randint
 
 import requests
 from django.conf import settings
@@ -11,8 +12,30 @@ def cats_check_status():
     pass
 
 
-def cats_submit_solution():
-    pass
+SIDs = [
+    'T2JHD2yqiK8Qhwy1h5I2fUWSH0V627',
+    'UwjGlps7GnxN3JPXuwlqqCDGAwTDpm',
+    'HDFM3NoMDSCLeneRf6bcY2LOQvvIDR',
+    'rLlFKX0mosoHnjWep0dPa92DqBaeSN',
+    'GEISsJMyAALx6KnAndOuwYqSmfbmal',
+]
+
+
+def cats_submit_solution(cats_user_id: int, source_text: str, problem_id: int, de_id: int, source=None):
+    # url = f'{settings.CATS_URL}?f=api_submit_problem;sid={cats_user_id}'
+    url = f'{settings.CATS_URL}main.pl?f=api_submit_problem;sid=GEISsJMyAALx6KnAndOuwYqSmfbmal'
+    data = {
+        'source': source,
+        'de_id': de_id,
+        'source_text': source_text,
+        'problem_id': problem_id
+    }
+    r = requests.post(url, data=data)
+    if r.status_code != 200:
+        raise CatsAnswerCodeException(r.reason)
+    r_content = json.loads(r.content.decode('utf-8'))
+    if hasattr(r_content, 'href_run_details'):
+        details = r_content['href_run_details']
 
 
 def cats_submit_problem():
@@ -31,6 +54,3 @@ def cats_get_problems_from_contest(contest_id, cats_user_id):
     data = json.loads(answer.content.decode('utf-8'))
     course_problems = CatsProblemSerializer(data=data, many=True)
     return course_problems
-
-
-
--- cathie/models.py
+++ cathie/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import User
+
 
 class CourseContestLink(models.Model):
     pass
@@ -14,4 +16,26 @@ class CatsProblem(models.Model):
 
 
 class CatsSubmit(models.Model):
-    pass
+    SUBMIT_STATUS = [
+        ('WA', 'Wrong answer'),
+        ('OK', 'OK'),
+        ('NP', 'NP'),
+        ('RJ', 'Rejected'),
+        ('CE', 'Compilation error'),
+        ('LI', 'Linter error'),
+        ('RE', 'Run-time error'),
+        ('PE', 'Presentation error'),
+        ('TL', 'Time limit exceeded'),
+        ('IL', 'Idleness limit exceeded'),
+        ('ML', 'Memory limit exceeded'),
+        ('WL', 'Write limit exceeded'),
+        ('SV', 'Security violation'),
+        ('IS', 'Ignored submit'),
+        ('AW', 'Awaiting manual verification'),
+        ('MR', 'Rejected by manual verification'),
+        ('BA', 'Banned')
+    ]
+    problem = models.ForeignKey(CatsProblem, on_delete=models.CASCADE, null=False)
+    student = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    source_text = models.TextField()
+    status = models.CharField(max_length=2, choices=SUBMIT_STATUS)
