d3b6269d298e8381fdb129d3e605cf906483b880
Darya Pakhomova <56807101+mrrlanie@users.noreply.github.com>
2021-01-20 17:19:41 +1000

    templates for (in-)valid link usage + (not doned yet) + (pathes too)

---
+++ frontend/src/views/InvalidLinkRegister.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1> Извините, здесь пусто :(</h1>
+    <h3> Обратитесь к преподавателю за новой ссылкой </h3>
+  </div>
+</template>
+
+<script>
+export default {
+name: "InvalidLinkRegister"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/ValidLinkRegister.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  dd
+</div>
+</template>
+
+<script>
+export default {
+  name: "RegisterCourse"
+}
+</script>
+
+<style scoped>
+
+</style>
