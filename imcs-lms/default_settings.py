c4017e55be4b5779b6e16e3c33f8e16a30e834c2
Main <maingartvlad@outlook.com>
2021-01-22 02:47:50 +1000

    default settings: celery params

--- imcs-lms/default_settings.py
+++ imcs-lms/default_settings.py
@@ -63,3 +63,11 @@ AUTH_USER_MODEL = 'users.User'
 CATS_URL = 'http://localhost:7999'
 CATS_SID = ''
 CATS_TOKEN = ''
+
+# Celery Configuration Options
+CELERY_TIMEZONE = 'Asia/Vladivostok'
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
