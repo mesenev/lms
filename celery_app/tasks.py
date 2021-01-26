66d53ae81f9cb3edad5f962a3aeffe73ca2937fe
Main <maingartvlad@outlook.com>
2021-01-27 00:24:14 +1000

    task update_submit_status: filter by status

--- celery_app/tasks.py
+++ celery_app/tasks.py
@@ -7,11 +7,15 @@ from problem.models import Submit
 
 logger = get_task_logger(__name__)
 
+PROCESSED_STATUSES = [status for status, description in Submit.SUBMIT_STATUS if status not in ('NP', 'AW')]
+
 
 @app.task
 def update_submit_status():
-    for submit in Submit.objects.filter(cats_request_id__isnull=False):
+    query = Submit.objects.filter(cats_request_id__isnull=False).exclude(status__in=PROCESSED_STATUSES)
+    for submit in query:
         new_status = cats_check_solution_status(submit.cats_request_id)
         if new_status is not None:
             submit.status = new_status
         submit.save()
+        print(submit.status)
