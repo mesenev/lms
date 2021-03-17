f202a1897acfe21af4ec635eadb4d833494e55ea
Pavel Mesenev <heredemonsnearby@mail.ru>
2021-03-17 18:59:08 +1000

    frontend tests cleanup (awaiting repair)

--- frontend/src/views/__tests__/HomeView.spec.ts
+++ frontend/src/views/__tests__/HomeView.spec.ts
@@ -1,11 +1,11 @@
-import { shallowMount } from '@vue/test-utils'
-import HomeView from '../HomeView.vue'
+import { shallowMount } from '@vue/test-utils';
+import HomeView from '../HomeView.vue';
 
 describe('HomeView.vue', () => {
   test('unit-test', () => {
     const wrapper = shallowMount(HomeView, {
-      propsData: {}
-    })
-    console.log(wrapper.html())
-  })
-})
+      propsData: {},
+    });
+    console.log(wrapper.html());
+  });
+});
--- frontend/src/views/__tests__/RegistrationView.spec.ts
+++ frontend/src/views/__tests__/RegistrationView.spec.ts
@@ -1,24 +1,24 @@
-import {shallowMount} from '@vue/test-utils'
-import RegistrationView from '../RegistrationView.vue'
+import { shallowMount } from '@vue/test-utils';
+import RegistrationView from '../RegistrationView.vue';
 
 describe('RegistrationView.vue', () => {
   test('Проверка работы полей ввода', () => {
-    const wrapper = shallowMount(RegistrationView)
+    const wrapper = shallowMount(RegistrationView);
     wrapper.setData({
       login: '1',
       first_name: '1',
       last_name: '1',
       email: '1',
       password: '1',
-      password_repeat: '1'
-    })
-    expect(wrapper.vm.$data.login).toBe('1')
-    expect(wrapper.vm.$data.first_name).toBe('1')
-    expect(wrapper.vm.$data.last_name).toBe('1')
-    expect(wrapper.vm.$data.email).toBe('1')
-    expect(wrapper.vm.$data.password).toBe('1')
-    expect(wrapper.vm.$data.password_repeat).toBe('1')
-  })
+      password_repeat: '1',
+    });
+    expect(wrapper.vm.$data.login).toBe('1');
+    expect(wrapper.vm.$data.first_name).toBe('1');
+    expect(wrapper.vm.$data.last_name).toBe('1');
+    expect(wrapper.vm.$data.email).toBe('1');
+    expect(wrapper.vm.$data.password).toBe('1');
+    expect(wrapper.vm.$data.password_repeat).toBe('1');
+  });
 
   test('Methods are rendered', () => {
     const wrapper = shallowMount(RegistrationView, {
@@ -30,21 +30,21 @@ describe('RegistrationView.vue', () => {
         checkPasswordLen: () => {},
         checkRepeatPassword: () => {},
         canAction: () => {},
-        action: () => {}
-      }
-    })
-    expect(wrapper.exists()).toBe(true)
-  })
+        action: () => {},
+      },
+    });
+    expect(wrapper.exists()).toBe(true);
+  });
 
   //TODO: check if action goes with btn click
   jest.mock("RegistrationView", () => ({
-    register(){
+    register() {
       return {
         onChange() {
-          return true
-        }
-      }
-    }
-  }))
-})
+          return true;
+        },
+      };
+    },
+  }));
+});
 
