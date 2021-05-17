c8dfef67fdfafc918ba84501c4610ef2ad25620b
mesenev <heredemonsnearby@mail.ru>
2021-05-17 18:44:43 +1000

    add worker launch to celery init script

--- .docker/conf/gunicorn/celery.sh
+++ .docker/conf/gunicorn/celery.sh
@@ -6,6 +6,9 @@ export PYTHONPATH=${DJANGODIR}:${PYTHONPATH}
 if [ -n "$NORELOAD" ]; then
 	    PARAMS="--noreload"
 fi
-celery -A celery_app beat --detach
-flower -A celery_app --port=7999
 
+
+
+celery -A celery_app beat &
+flower -A celery_app --port=7999 &
+celery -A celery_app worker -c 2 -O fair
