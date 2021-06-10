841d4e4cc6023cff85e38b0ac8b386b49b49ff93
Pavel Mesenev <heredemonsnearby@mail.ru>
2021-06-10 19:40:56 +1000

    user-component id prop support & non-student-for-course support

--- frontend/src/components/UserComponent.vue
+++ frontend/src/components/UserComponent.vue
@@ -6,31 +6,54 @@
            alt=""
            width="30"
            height="30">
-      <span class="name">{{ name }}</span>
+      <cv-tooltip v-if="warning" tip="Пользователь не является студентом курса"/>
+      <div class="name">
+        <cv-loading v-if="loading" :small="true"/>
+        <span v-else>{{ name }}</span>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import UserModel from "@/models/UserModel";
+import userStore from '@/store/modules/user';
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
 
 @Component({ components: {} })
 export default class UserComponent extends Vue {
-  @Prop({ required: true }) user!: UserModel;
-  src = "https://www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png";
+  @Prop({ required: false }) user!: UserModel;
+  @Prop({ required: false }) userId!: number;
+  loading = true;
+  warning = false;
+  userStore = userStore;
+  _user!: UserModel;
 
   get name() {
-    if (this.user && this.user.first_name && this.user.last_name)
-      return `${this.user.first_name} ${this.user.last_name}`;
-    return this.user.username;
+    if (this._user && this._user.first_name && this._user.last_name)
+      return `${this._user.first_name} ${this._user.last_name}`;
+    return '';
   }
 
   get pic_url() {
-    if (!this.user.avatar_url)
-      return this.src;
-    return this.user.thumbnail;
+    if (this._user && this._user.avatar_url)
+      return this._user.thumbnail;
+    return "https://www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png";
+  }
+
+  async created() {
+    if (!this.user && !this.userId)
+      throw {};
+    if (this.user)
+      this._user = this.user;
+    if (this.userId)
+      this._user = this.userStore.currentCourseStudents[this.userId];
+    if (typeof this._user === 'undefined') {
+      this.warning = true;
+      this._user = await this.userStore.fetchUserById(this.userId)
+    }
+    this.loading = false;
   }
 }
 </script>
@@ -45,7 +68,7 @@ export default class UserComponent extends Vue {
   align-items center
 
 .avatar
-  object-fit:cover;
+  object-fit: cover;
   border-radius: 150%;
   padding: 0;
   margin: 0;
