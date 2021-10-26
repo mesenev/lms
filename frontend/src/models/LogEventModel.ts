615397dec473c1db82d432f4e8472de8b3d6554f
dreamerror <66074257+dreamerror@users.noreply.github.com>
2021-10-26 21:04:08 +1000

    add LogEventModel

---
+++ frontend/src/models/LogEventModel.ts
@@ -0,0 +1,12 @@
+import { BaseModel } from '@/models/BaseModel';
+import UserModel from "@/models/UserModel";
+
+
+export default interface LogEventModel extends BaseModel {
+  type: string;
+  text: string;
+  sender: UserModel;
+  submitId?: number;
+  lessonId: number;
+  courseId: number;
+}
