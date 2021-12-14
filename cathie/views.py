a42f441930b643b49933f4ef6c07eab149f8725c
mesenev <heredemonsnearby@mail.ru>
2021-12-14 14:21:49 +1000

    decorations for cathie-api

--- cathie/views.py
+++ cathie/views.py
@@ -1,43 +1,13 @@
-import json
-
-import requests
-from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 
-from cathie import authorization
+from cathie.authorization import *
 from cathie.cats_api import cats_get_problems_from_contest, cats_get_problem_description_by_url
-from cathie.exceptions import CatsAnswerCodeException, CatsAuthorizationException
 from course.models import Course
 from problem.models import Problem
-
-from django.shortcuts import render
-from cathie.authorization import *
-
-
-def check_authorization_for_cats(function_to_decorate):
-    def wrapper(*args, **kwargs):
-        url = f'{settings.CATS_URL}?sid={authorization.cats_sid()};json=1'
-        r = requests.get(url)
-        if r.status_code != 200:
-            raise CatsAnswerCodeException(r.reason)
-        if 'error' in json.loads(r.content.decode('utf-8')):
-            auth = requests.post(
-                f'{settings.CATS_URL}?f=login;json=1',
-                {'login': settings.CATS_LOGIN, 'passwd': settings.CATS_PASSWD}
-            )
-            if auth.status_code != 200:
-                raise CatsAnswerCodeException(r.reason)
-            try:
-                content = json.loads(auth.content.decode('utf-8'))
-            except:
-                raise CatsAuthorizationException('Invalid json from cats')
-            authorization.cats_sid_setter(content['sid'])
-        return function_to_decorate(*args, **kwargs)
-
-    return wrapper
 
 
 @login_required
