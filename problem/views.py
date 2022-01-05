4838a44a77e4dc09ebc0db8507c3322708119d73
mesenev <heredemonsnearby@mail.ru>
2022-01-05 23:50:41 +1000

    change default pagination page size

--- problem/views.py
+++ problem/views.py
@@ -70,7 +70,7 @@ class ProblemViewSet(viewsets.ModelViewSet):
 
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 2
+    page_size = 25
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
