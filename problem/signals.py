af135058d133b18e4389b16fc27ff323b8d27140
mesenev <heredemonsnearby@mail.ru>
2022-04-13 16:50:33 +1000

    .

--- problem/signals.py
+++ problem/signals.py
@@ -1,8 +1,6 @@
-from channels.layers import get_channel_layer
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.db import models
-from django.core.management import BaseCommand
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 
