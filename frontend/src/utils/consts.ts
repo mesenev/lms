afab6339c0e60f5d6892c478d10656ecfd69d584
mesenev <heredemonsnearby@mail.ru>
2022-07-30 16:08:30 +1000

    de_id fix

--- frontend/src/utils/consts.ts
+++ frontend/src/utils/consts.ts
@@ -1,10 +1,10 @@
 export const de_options = [
   {
-    value: '4', label: 'Cross-platform C/C++ compiler',
+    value: '3', label: 'Cross-platform C/C++ compiler',
     name: 'Cross-platform C/C++ compiler', disabled: false,
   },
   {
-    value: '502', label: 'Python 3.8.1',
+    value: '681949', label: 'Python 3.8.1',
     name: 'Python 3.8.1', disabled: false,
   },
 ];
