26e74109af96868e2ce65d2969cc0ae455fa5461
mesenev <heredemonsnearby@mail.ru>
2022-08-02 15:10:12 +1000

    add trusted_origins settings

--- imcslms/default_settings.py
+++ imcslms/default_settings.py
@@ -15,6 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 DEBUG = os.getenv('DJANGO_DEBUG')
 ALLOWED_HOSTS = [os.getenv('DJANGO_ALLOWED_HOSTS'), ]
+CSRF_TRUSTED_ORIGINS = [os.getenv('DJANGO_CSRF_TRUSTED_ORIGINS'), ]
 
 INTERNAL_IPS = ["127.0.0.1", '*']
 
