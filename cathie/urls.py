182dd180661476e624055ce6574e2df78cd61286
froggling <ilya.laguxa@gmail.com>
2022-11-03 00:18:01 +1000

    make CatsContest APIView class instead of api_view decorator

--- cathie/urls.py
+++ cathie/urls.py
@@ -6,7 +6,6 @@ from cathie import views
 urlpatterns = [
     path('api/cats-problems/<int:course_id>/', views.ListCatsProblems.as_view(), name='cats-problems'),
     path('api/cats-problem-description/<int:problem_id>/', views.ProblemDescription.as_view()),
-    path('api/cats-contests/', views.get_cats_contests, name='cats-contests'),
-    path('api/add-users-to-contest/', views.add_users_to_contest, name='add-users-to-contest'),
+    path('api/cats-contests/', views.CatsContest.as_view(), name='cats-contests'),
     path('admin/cats/', views.cats_admin, name='cats-admin'),
 ]
