254f49e59d0a96775f384573763af4b4429140f9
TCKACHIKSIS <71429999+TCKACHIKSIS@users.noreply.github.com>
2022-11-04 15:31:38 +1000

    bring changes to branch

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -30,7 +30,6 @@ import LogoGithub from '@carbon/icons-vue/es/logo--github/16';
 import { Component, Vue, Watch } from 'vue-property-decorator';
 import LoginView from "@/views/LoginView.vue";
 import tokenStore from "@/store/modules/token";
-
 @Component({ components: { LoginView, LmsHeader, LmsBreadcrumb, LogoGithub } })
 export default class App extends Vue {
   //TODO set transition and styles for loader
@@ -42,15 +41,12 @@ export default class App extends Vue {
       this.$router.push('/login');
     }
   }
-
   get isLoading() {
     return tokenStore.isLoading;
   }
-
   get isLogin() {
     return tokenStore.isAuthenticated;
   }
-
   async created() {
     await tokenStore.setupTokenStore();
   }
@@ -60,55 +56,42 @@ export default class App extends Vue {
 <style lang="scss">
 @import "styles/base";
 @import "styles/carbon";
-
 .fade-enter-active, .fade-leave-active {
   transition: opacity .1s;
 }
-
 .fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */
 {
   opacity: 0;
 }
-
 </style>
 
 <style scoped lang="stylus">
 @import 'styles/list-elements.styl';
-
-
 .main--breadcrumb
   margin-top var(--cds-spacing-06);
-
 .items
   display flex
   flex-wrap wrap
-
 .item
   width: 60%
   color: var(--cds-support-02)
-
 .layout
   height: 100%
   display flex
   flex-flow column
-
   &-content
     background-color var(--cds-ui-01)
     padding-bottom var(--cds-spacing-05)
     margin-top: 3rem;
-
   &-header, &-footer
     flex-shrink 0
-
   &-footer
     min-height 100px
     background-color var(--cds-ui-05)
     color var(--cds-text-05)
     font-size 0.7em
-
     &-label
       margin var(--cds-spacing-06) var(--cds-spacing-06)
-
   &-content
     flex-grow 1
     width: 100%
@@ -117,4 +100,5 @@ export default class App extends Vue {
 <style lang="stylus">
 @import "styles/list-elements";
 </style>
+
 
