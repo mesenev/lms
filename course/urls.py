6a5d3877e6df5f8680149dcd3543529a5f7ac5bf
mesenev <heredemonsnearby@mail.ru>
2022-11-08 18:25:41 +1000

    fix: urls for course-registration-link

--- course/urls.py
+++ course/urls.py
@@ -4,6 +4,6 @@ from course import views
 
 urlpatterns = [
     path('api/check-link/<str:link>/', views.CheckLinkApi.as_view(), name='check-link'),
-    path('api/delete-link/<str:link>/', views.CourseRegistrationApi.as_view(), name='delete-link'),
-    path('api/course-registration/<str:link>/', views.LinkDeletionAPi.as_view(), name='course-registration'),
+    path('api/delete-link/<str:link>/', views.LinkDeletionAPi.as_view(), name='delete-link'),
+    path('api/course-registration/<str:link>/', views.CourseRegistrationApi.as_view(), name='course-registration'),
 ]
