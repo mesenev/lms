784347ad5b957f5dd40df2a52c50cb82ede53229
Yasuhiro-gh <pavlich_eng00@mail.ru>
2022-11-15 20:39:25 +1000

    staff flag for course deleting

--- frontend/src/components/lists/CourseListComponent.vue
+++ frontend/src/components/lists/CourseListComponent.vue
@@ -6,7 +6,7 @@
       <h5 class="list-element--title">{{ course.name }}</h5>
       <span class="list-element--info">Преподаватель: {{ teacher }}</span>
     </div>
-    <div class="list-element--btns list-element">
+    <div class="list-element--btns list-element" v-if="isStaff">
       <component class="trash-icon" :is="TrashCan" @click.prevent.stop="deleteCourse(course)"/>
       <component class="settings-icon" :is="Settings" @click.prevent.stop="editCourse(course)"/>
     </div>
@@ -20,12 +20,14 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 import TrashCan from '@carbon/icons-vue/es/trash-can/20';
 import Settings from '@carbon/icons-vue/es/settings/20';
 import router from "@/router";
+import userStore from '@/store/modules/user';
 
 @Component
 export default class CourseListComponent extends Vue {
   @Prop({ required: true }) courseProp!: CourseModel;
   TrashCan = TrashCan;
   Settings = Settings;
+  userStore = userStore;
 
   get course(): CourseModel {
     return this.courseProp;
@@ -39,6 +41,10 @@ export default class CourseListComponent extends Vue {
         + `${this.course.author.middle_name} `
         + `${this.course.author.last_name}`;
     return `${this.course.author.first_name} ${this.course.author.last_name}`;
+  }
+
+  get isStaff() {
+    return this.userStore.user.staff_for.includes(this.course.id);
   }
 
   editCourse(course: CourseModel) {
