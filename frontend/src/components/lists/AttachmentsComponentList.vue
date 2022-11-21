f55978bee55df7251f393e7a8777707717a8f6c6
TCKACHIKSIS <71429999+TCKACHIKSIS@users.noreply.github.com>
2022-11-21 19:44:51 +1000

    add attachmentView

--- frontend/src/components/lists/AttachmentsComponentList.vue
+++ frontend/src/components/lists/AttachmentsComponentList.vue
@@ -1,20 +1,27 @@
 <template>
-  <div v-html="this.file"></div>
+  <div>
+    {{attachment.name}}
+    <component class="trash-icon" :is="TrashCan" @click.prevent.stop="deleteAttachment"/>
+  </div>
 </template>
 
 <script lang="ts">
-import marked from 'marked';
 import {Component, Prop} from 'vue-property-decorator';
 import NotificationMixinComponent from '../common/NotificationMixinComponent.vue';
-import MaterialFileModel from "@/models/Attachment";
+import AttachmentModel from "@/models/Attachment";
+import TrashCan from '@carbon/icons-vue/es/trash-can/20';
+import MaterialStore from '@/store/modules/material'
 @Component({
-  components: {
-
-  }
+  components: {}
 })
 export default class FileListComponent extends NotificationMixinComponent {
-  @Prop( { required: true } ) file!: MaterialFileModel;
+  @Prop( { required: true } ) attachment!: AttachmentModel;
+  TrashCan = TrashCan;
+  materialStore = MaterialStore
 
+  deleteAttachment(){
+    this.materialStore.deleteAttachment(this.attachment.id);
+  }
 }
 </script>
 <style lang="stylus">
