79871f6e5e9a43ca9ec2bb2e500c7cbba601d34e
Yasuhiro-gh <pavlich_eng00@mail.ru>
2022-11-24 03:07:53 +1000

    styles + confirm deletion

--- frontend/src/components/lists/AttachmentsComponentList.vue
+++ frontend/src/components/lists/AttachmentsComponentList.vue
@@ -1,51 +1,69 @@
 <template>
-  <div>
-    {{attachment.name}}
-    <component class="trash-icon" :is="TrashCan" @click.prevent.stop="deleteAttachment"/>
-    <Insert @click="insertAttachment" />
+  <div class="attachment-content">
+    <div class="attachment-title">
+    {{ attachment.name }}
+    </div>
+    <div class="attachment-btns">
+      <component class="trash-icon icon" :is="TrashCan" @click.prevent.stop="deleteAttachment"/>
+      <component class="copy-icon icon" :is="Copy" title="Скопировать" @click="insertAttachment"/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import {Component, Prop} from 'vue-property-decorator';
+import { Component, Prop } from 'vue-property-decorator';
 import NotificationMixinComponent from '../common/NotificationMixinComponent.vue';
 import AttachmentModel from "@/models/Attachment";
 import TrashCan from '@carbon/icons-vue/es/trash-can/20';
-import Insert from '@carbon/icons-vue/es/insert/20'
-import MaterialStore from '@/store/modules/material'
-import marked from 'marked';
+import Copy from '@carbon/icons-vue/es/copy/20'
+
 @Component({
-  components: {Insert}
+  components: { Copy, }
 })
 export default class FileListComponent extends NotificationMixinComponent {
-  @Prop( { required: true } ) attachment!: AttachmentModel;
+  @Prop({ required: true }) attachment!: AttachmentModel;
   TrashCan = TrashCan;
-  materialStore = MaterialStore
+  Copy = Copy;
 
-  deleteAttachment(){
-    this.materialStore.deleteAttachment(this.attachment.id);
-  }
-  insertAttachment(){
+  insertAttachment() {
     let markdown_file_string = '';
 
-    if (this.attachment.file_format.includes('image/')){
-      markdown_file_string = '<img src="' +  this.attachment.file_url + '" alt="' +
-      this.attachment.name;
-      markdown_file_string += '" width="500" height="600">';
-    }
-    else if(this.attachment.file_format.includes('video/')){
-      markdown_file_string = '<video><source src="' + this.attachment.file_url
-      + ' " controls></video>'
-    }
-    else {
+    if (this.attachment.file_format.includes('image/')) {
+      markdown_file_string = '<img src="' + this.attachment.file_url + '" alt="' +
+        this.attachment.name;
+      markdown_file_string += '" width="" height="">';
+    } else if (this.attachment.file_format.includes('video/')) {
+      markdown_file_string = '<video controls="controls"><source src="' + this.attachment.file_url
+        + ' "></video>'
+    } else {
       markdown_file_string = '<a href="' + this.attachment.file_url + '">' + this.attachment.name
-      + '</a>'
+        + '</a>'
     }
+    window.navigator.clipboard.writeText(markdown_file_string);
+  }
 
-    window.navigator.clipboard.writeText(markdown_file_string)
+  deleteAttachment() {
+    this.$emit('show-confirm-modal', this.attachment);
   }
 }
 </script>
 <style lang="stylus">
+.attachment-content
+  display flex
+  flex-direction row
+  justify-content space-between
+  margin 0.5rem
+
+.attachment-title
+  display flex
+  align-items center
+
+.trash-icon
+  margin-right 0.5rem
+
+.icon
+  transition ease-in-out 0.1s
+.icon:active
+  transform scale(0.9)
 
 </style>
