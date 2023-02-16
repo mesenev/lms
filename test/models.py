5a3e7887e8fc69a4acc619b562dffb107b7e1087
TCKACHIKSIS <71429999+zogich@users.noreply.github.com>
2023-02-16 14:56:12 +1000

    fix permissions

--- test/models.py
+++ test/models.py
@@ -3,8 +3,15 @@ from lesson.models import Lesson
 
 
 class Test(models.Model):
+    TEST_MODE_TYPES = [
+        ('auto', 'Automated only problem testing'),
+        ('manual', 'Manual only problem testing'),
+        ('auto_and_manual', 'Manual then automated problem testing')
+    ]
+
     name = models.CharField(max_length=500)
     lesson = models.ForeignKey(Lesson, on_delete=models.SET_NULL, related_name='tests', null=True)
+    test_mode = models.CharField(max_length=30, choices=TEST_MODE_TYPES, default=TEST_MODE_TYPES[0][0])
 
     def __str__(self):
         return self.name
