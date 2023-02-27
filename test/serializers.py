3422f30dfa626077e491bfb345da19fd66b2dece
TCKACHIKSIS <71429999+zogich@users.noreply.github.com>
2023-02-27 20:54:47 +1000

    add test solution api

--- test/serializers.py
+++ test/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from test.models import Test
+from test.models import Test, TestSolution
 from django.db import models
 from lesson.models import Lesson
 
@@ -10,4 +10,13 @@ class TestSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Test
+        fields = '__all__'
+
+
+class TestSolutionSerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField()
+    name = serializers.CharField()
+
+    class Meta:
+        model = TestSolution
         fields = '__all__'
