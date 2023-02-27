02e5c356f15264c3ccf679e3003b6d8aa9d3cd55
TCKACHIKSIS <71429999+zogich@users.noreply.github.com>
2023-02-28 01:24:27 +1000

    fix question field

--- test/models.py
+++ test/models.py
@@ -1,11 +1,10 @@
-import typing
-
 from django.db import models
 from django.contrib import admin
 from lesson.models import Lesson
 import pydantic
 from django_pydantic_field import SchemaField
 from users.models import User
+from pydantic import validate_arguments
 
 
 class Answer(pydantic.BaseModel):
@@ -15,7 +14,7 @@ class Answer(pydantic.BaseModel):
 
 class Question(pydantic.BaseModel):
     text: str
-    description: str
+    description: str = ''
     correct_answers: list[str] = []
     all_answers: list[str] = []
     answer_type: str
@@ -29,13 +28,19 @@ class Test(models.Model):
         ('manual', 'Manual only testing'),
         ('auto_and_manual', 'Manual then automated testing')
     ]
-    description = models.TextField(default='')
-    questions: typing.Sequence[Question] = SchemaField(schema=list[Question])
-    points = models.IntegerField()
     name = models.CharField(max_length=500)
     lesson = models.ForeignKey(Lesson, on_delete=models.SET_NULL, related_name='tests', null=True)
+    description = models.TextField(default='')
+    questions = models.JSONField()
+    points = models.IntegerField()
     is_hidden = models.BooleanField(default=True)
     test_mode = models.CharField(max_length=30, choices=TEST_MODE_TYPES, default=TEST_MODE_TYPES[0][0])
+
+    @classmethod
+    @validate_arguments
+    def create(cls, name, lesson, description, questions: list[Question], points, is_hidden, test_mode):
+        test = cls(name, lesson, description, questions, points, is_hidden, test_mode)
+        return test
 
     def __str__(self):
         return self.name
--- test/serializers.py
+++ test/serializers.py
@@ -1,14 +1,11 @@
 from rest_framework import serializers
 from test.models import Test, TestSolution, Question
 from django_pydantic_field import SchemaField
-from django.db import models
-from lesson.models import Lesson
 
 
 class TestSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
     name = serializers.CharField()
-    questions = SchemaField(schema=Question)
 
     class Meta:
         model = Test
