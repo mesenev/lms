4b62591acc9b3f4f910daac9d37c8b188851d501
TCKACHIKSIS <71429999+zogich@users.noreply.github.com>
2023-02-27 19:12:17 +1000

    upd field for test model

--- test/models.py
+++ test/models.py
@@ -10,22 +10,29 @@ class Answer(pydantic.BaseModel):
 
 
 class Question(pydantic.BaseModel):
+    test: int
     text: str
+    description: str
     correct_answers: list[Answer] = []
     all_answers: list[Answer] = []
+    answer_type: str
     attachment_url: str = ''
+    points: int
 
 
 class Test(models.Model):
-
     TEST_MODE_TYPES = [
         ('auto', 'Automated only testing'),
         ('manual', 'Manual only testing'),
         ('auto_and_manual', 'Manual then automated testing')
     ]
-    question_list: list[Question] = SchemaField(default=[])
+    description = models.TextField(default='')
+    questions: list[Question] = SchemaField(default=[])
+    points = models.IntegerField()
     name = models.CharField(max_length=500)
     lesson = models.ForeignKey(Lesson, on_delete=models.SET_NULL, related_name='tests', null=True)
+    is_hidden = models.BooleanField(default=True)
+    test_mode = models.CharField(max_length=30, choices=TEST_MODE_TYPES, default=TEST_MODE_TYPES[0][0])
 
     def __str__(self):
         return self.name
