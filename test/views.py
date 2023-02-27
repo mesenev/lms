9ca6b4db99cd0e018e63394bb9e53f75866e17fc
TCKACHIKSIS <71429999+zogich@users.noreply.github.com>
2023-02-27 20:54:47 +1000

    add test solution api

--- test/views.py
+++ test/views.py
@@ -1,9 +1,9 @@
 from rest_framework import viewsets, exceptions
 from users.permissions import CourseStaffOrReadOnlyForStudents
-from test.models import Test
+from test.models import Test, TestSolution
 from django.db.models import Q
 from imcslms.default_settings import TEACHER
-from test.serializers import TestSerializer
+from test.serializers import TestSerializer, TestSolutionSerializer
 from model_bakery import baker
 from rest_framework.request import Request
 from rest_framework.views import APIView
@@ -31,3 +31,27 @@ class TestViewSet(viewsets.ModelViewSet):
         if request.user.groups.filter(name=TEACHER).exists():
             return super().create(request, *args, **kwargs)
         raise exceptions.PermissionDenied
+
+
+class TestSolutionViewSet(viewsets.ModelViewSet):
+    serializer_class = TestSolutionSerializer
+    permission_classes = [CourseStaffOrReadOnlyForStudents]
+    filterset_fields = ['test', ]
+
+    def get_queryset(self):
+        user = self.request.user
+        return TestSolution.objects.all().filter(
+            (Q(test__is_hidden=False)
+             & Q(test__lesson__course__in=user.student_for.all())
+             )
+            | Q(test__lesson__course__in=user.staff_for.all())
+            | Q(test__lesson__course__in=user.author_for.all())
+        )
+
+    def create(self, request, *args, **kwargs):
+        if request.user.groups.filter(name=TEACHER).exists():
+            return super().create(request, *args, **kwargs)
+        raise exceptions.PermissionDenied
+
+    def filter_queryset(self, queryset):
+        return super().filter_queryset(queryset)
