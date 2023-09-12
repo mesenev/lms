4b6f1bdc5dbd3999824e7592ebee3750d78072d0
zogich <tkachenko.ei@students.dvfu.ru>
2023-09-12 18:57:54 +1000

    create problem routes

---
+++ frontend_vue3/src/router/problem.ts
@@ -0,0 +1,13 @@
+import ProblemView from '@/views/ProblemView.vue';
+
+const problemRoutes = [
+  {
+    path: '',
+    name: 'ProblemView',
+    component: ProblemView,
+  },
+
+];
+
+
+export default problemRoutes;
