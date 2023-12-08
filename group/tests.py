1e56aa7251a01942cae34158c5117c77bf477e02
zogich <tkachenko.ei@students.dvfu.ru>
2023-12-08 21:57:20 +1000

    add some test for groups

--- group/tests.py
+++ group/tests.py
@@ -6,10 +6,12 @@ from group.serializers import GroupSerializer
 from model_bakery import baker
 from django.urls import reverse
 from rest_framework import status
+from users.models import GroupAssignTeacher
+from users.serializers import DefaultUserSerializer
 
 
 class ProblemTests(MainSetup):
-     def test_create_problem(self):
+     def test_create_group(self):
          self.test_setup()
          (course := baker.make(Course)).save()
          instance = baker.make(Group)
@@ -21,3 +23,30 @@ class ProblemTests(MainSetup):
          response = self.client.post(url, data, format='json')
          self.assertEqual(response.status_code, status.HTTP_201_CREATED)
          self.assertEqual(Group.objects.count(), amount + 1)
+
+     def test_assign_teacher(self):
+
+         self.test_setup()
+         (group := baker.make(Group)).save()
+
+         url = reverse('group-detail', kwargs=dict(pk=group.id)) + "assign-teacher/"
+         data = DefaultUserSerializer(self.user).data
+         response = self.client.post(url, data, format='json')
+
+         self.assertEqual(response.status_code, status.HTTP_200_OK)
+         self.assertEqual(Group.objects.first(), self.user.staff_for.first())
+         self.assertEqual(self.user, Group.objects.first().staff.first())
+
+     def test_assign_student(self):
+         self.test_setup(group='student')
+         (group := baker.make(Group)).save()
+
+         url = reverse('group-detail', kwargs=dict(pk=group.id)) + "assign-student/"
+         data = DefaultUserSerializer(self.user).data
+         response = self.client.post(url, data, format='json')
+
+         self.assertEqual(response.status_code, status.HTTP_200_OK)
+         self.assertEqual(Group.objects.first(), self.user.student_for.first())
+         self.assertEqual(self.user, Group.objects.first().students.first())
+
+
