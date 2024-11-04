172733b29df72b6f0d8a7153489078ba630be071
Salomanka <oleg65play@gmail.com>
2024-11-04 18:11:20 +1000

    requirements and tests

--- group/tests.py
+++ group/tests.py
@@ -10,129 +10,123 @@ from users.models import CourseGroupAssignTeacher, User, CourseGroupAssignStuden
 from users.serializers import DefaultUserSerializer
 from django.contrib.auth.models import Group as PermissionsGroup
 
-
 class GroupTests(MainSetup):
-     def test_create_group(self):
-         self.test_setup()
-         baker.make(Course).save()
-         instance = baker.make(CourseGroup)
-         instance.course = Course.objects.first()
-         instance.save()
-         data = CourseGroupSerializer(instance).data
-         url = reverse('group-list')
-         amount = CourseGroup.objects.count()
-         response = self.client.post(url, data, format='json')
-         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-         self.assertEqual(CourseGroup.objects.count(), amount + 1)
+    def test_create_group(self):
+        self.test_setup()
+        baker.make(Course).save()
+        instance = baker.make(CourseGroup)
+        instance.course = Course.objects.first()
+        instance.save()
+        data = CourseGroupSerializer(instance).data
+        url = reverse('group-list')
+        amount = CourseGroup.objects.count()
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(CourseGroup.objects.count(), amount + 1)
 
-     def test_assign_teacher(self):
-         self.test_setup()
+    def test_assign_teacher(self):
+        self.test_setup()
 
-         baker.make(Course).save()
-         group = baker.make(CourseGroup)
-         group.course = Course.objects.first()
-         group.save()
+        baker.make(Course).save()
+        group = baker.make(CourseGroup)
+        group.course = Course.objects.first()
+        group.save()
 
-         CourseGroupAssignTeacher.objects.create(user=self.user, group=group).save()
+        CourseGroupAssignTeacher.objects.create(user=self.user, group=group).save()
 
-         new_teacher_user=baker.make(User)
-         PermissionsGroup.objects.get(name='teacher').user_set.add(new_teacher_user)
+        new_teacher_user=baker.make(User)
+        PermissionsGroup.objects.get(name='teacher').user_set.add(new_teacher_user)
 
-         url = reverse('group-detail', kwargs=dict(pk=group.id)) + "assign-teacher/"
-         data = DefaultUserSerializer(new_teacher_user).data
-         response = self.client.post(url, data, format='json')
+        url = reverse('group-detail', kwargs=dict(pk=group.id)) + "assign-teacher/"
+        data = DefaultUserSerializer(new_teacher_user).data
+        response = self.client.post(url, data, format='json')
 
-         self.assertEqual(response.status_code, status.HTTP_200_OK)
-         self.assertEqual(CourseGroup.objects.first(), self.user.staff_for.first())
-         self.assertEqual(self.user, CourseGroup.objects.first().staff.first())
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(CourseGroup.objects.first(), self.user.staff_for.first())
+        self.assertEqual(self.user, CourseGroup.objects.first().staff.first())
 
-     def test_assign_student(self):
-         self.test_setup()
+    def test_assign_student(self):
+        self.test_setup()
 
-         baker.make(Course).save()
-         group = baker.make(CourseGroup)
-         group.course = Course.objects.first()
-         group.save()
+        baker.make(Course).save()
+        group = baker.make(CourseGroup)
+        group.course = Course.objects.first()
+        group.save()
 
-         CourseGroupAssignTeacher.objects.create(user=self.user, group=group).save()
+        CourseGroupAssignTeacher.objects.create(user=self.user, group=group).save()
 
-         new_student_user = baker.make(User)
-         PermissionsGroup.objects.get(name='student').user_set.add(new_student_user)
+        new_student_user = baker.make(User)
+        PermissionsGroup.objects.get(name='student').user_set.add(new_student_user)
 
-         url = reverse('group-detail', kwargs=dict(pk=group.id)) + "assign-student/"
-         data = DefaultUserSerializer(new_student_user).data
-         response = self.client.post(url, data, format='json')
+        url = reverse('group-detail', kwargs=dict(pk=group.id)) + "assign-student/"
+        data = DefaultUserSerializer(new_student_user).data
+        response = self.client.post(url, data, format='json')
 
-         new_student_user = User.objects.get(id=new_student_user.id)
+        new_student_user = User.objects.get(id=new_student_user.id)
 
-         self.assertEqual(response.status_code, status.HTTP_200_OK)
-         self.assertEqual(CourseGroup.objects.first(), new_student_user.student_for.first())
-         self.assertEqual(new_student_user, CourseGroup.objects.first().students.first())
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(CourseGroup.objects.first(), new_student_user.student_for.first())
+        self.assertEqual(new_student_user, CourseGroup.objects.first().students.first())
 
-     def test_delete_teacher_from_group(self):
-         self.test_setup()
+    def test_delete_teacher_from_group(self):
+        self.test_setup()
 
-         baker.make(Course).save()
-         group = baker.make(CourseGroup)
-         group.course = Course.objects.first()
-         group.save()
+        baker.make(Course).save()
+        group = baker.make(CourseGroup)
+        group.course = Course.objects.first()
+        group.save()
 
-         CourseGroupAssignTeacher.objects.create(user=self.user, group=group).save()
+        CourseGroupAssignTeacher.objects.create(user=self.user, group=group).save()
 
-         new_teacher_user = baker.make(User)
-         PermissionsGroup.objects.get(name='teacher').user_set.add(new_teacher_user)
-         CourseGroupAssignTeacher.objects.create(user=new_teacher_user, group=group).save()
-         count = CourseGroupAssignTeacher.objects.count()
+        new_teacher_user = baker.make(User)
+        PermissionsGroup.objects.get(name='teacher').user_set.add(new_teacher_user)
+        CourseGroupAssignTeacher.objects.create(user=new_teacher_user, group=group).save()
+        count = CourseGroupAssignTeacher.objects.count()
 
-         url = reverse('group-delete-teacher', kwargs=dict(pk=group.id, user_id=new_teacher_user.id))
+        url = reverse('group-delete-teacher', kwargs=dict(pk=group.id, user_id=new_teacher_user.id))
 
-         data = DefaultUserSerializer(new_teacher_user).data
-         response = self.client.delete(url, data, format='json')
+        data = DefaultUserSerializer(new_teacher_user).data
+        response = self.client.delete(url, data, format='json')
 
-         self.assertEqual(response.status_code, status.HTTP_200_OK)
-         self.assertEqual(count-1, CourseGroupAssignTeacher.objects.count())
-         self.assertEqual(CourseGroup.objects.get(id=group.id).staff.count(), 1)
-         self.assertEqual(User.objects.get(id=new_teacher_user.id).staff_for.count(), 0)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(count-1, CourseGroupAssignTeacher.objects.count())
+        self.assertEqual(CourseGroup.objects.get(id=group.id).staff.count(), 1)
+        self.assertEqual(User.objects.get(id=new_teacher_user.id).staff_for.count(), 0)
 
-     def test_delete_student_from_group(self):
-         self.test_setup()
-         baker.make(Course).save()
-         group = baker.make(CourseGroup)
-         group.course = Course.objects.first()
-         group.save()
+    def test_delete_student_from_group(self):
+        self.test_setup()
+        baker.make(Course).save()
+        group = baker.make(CourseGroup)
+        group.course = Course.objects.first()
+        group.save()
 
-         CourseGroupAssignTeacher.objects.create(user=self.user, group=group).save()
+        CourseGroupAssignTeacher.objects.create(user=self.user, group=group).save()
 
-         new_student_user = baker.make(User)
-         PermissionsGroup.objects.get(name='student').user_set.add(new_student_user)
+        new_student_user = baker.make(User)
+        PermissionsGroup.objects.get(name='student').user_set.add(new_student_user)
 
-         CourseGroupAssignStudent.objects.create(user=new_student_user, group=group).save()
+        CourseGroupAssignStudent.objects.create(user=new_student_user, group=group).save()
 
-         data = DefaultUserSerializer(new_student_user).data
-         url = reverse('group-delete-student', kwargs=dict(pk=group.id, user_id=new_student_user.id))
-         response = self.client.delete(url, data, format='json')
+        data = DefaultUserSerializer(new_student_user).data
+        url = reverse('group-delete-student', kwargs=dict(pk=group.id, user_id=new_student_user.id))
+        response = self.client.delete(url, data, format='json')
 
-         self.assertEqual(response.status_code, status.HTTP_200_OK)
-         self.assertEqual(User.objects.get(id=new_student_user.id).student_for.count(), 0)
-         self.assertEqual(CourseGroup.objects.get(id=group.id).students.count(), 0)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(User.objects.get(id=new_student_user.id).student_for.count(), 0)
+        self.assertEqual(CourseGroup.objects.get(id=group.id).students.count(), 0)
 
-
-
-
-
-
-     # def test_create_link(self):
-     #     self.test_setup(group='teacher')
-     #     (group:= baker.make(Group)).save()
-     #     instanse = baker.make(GroupLink)
-     #     instanse.group = group
-     #
-     #     data = LinkSerializer(instanse).data
-     #     url = reverse('group-list')
-     #     response = self.client.post(url, data, format='json')
-     #     l = [link for link in GroupLink.objects.all()]
-     #     for item in l:
-     #         print(LinkSerializer(item).data)
-     #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-     #     self.assertEqual(group.id, GroupLink.objects.first().group.id)
+    def test_create_link(self):
+        self.test_setup(group='teacher')
+        (group:= baker.make(CourseGroup)).save()
+        instanse = baker.make(CourseGroupLink)
+        instanse.group = group
+    
+        data = CourseGroupLinkSerializer(instanse).data
+        url = reverse('group-list')
+        response = self.client.post(url, data, format='json')
+        l = [link for link in CourseGroupLink.objects.all()]
+        for item in l:
+            print(CourseGroupLinkSerializer(item).data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(group.id, CourseGroupLink.objects.first().group.id)
 
