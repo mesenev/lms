a0c24d5128db9dfe1cd44a7f70bd06929fe98b35
egor <tckachiksis@gmail.com>
2026-04-13 15:02:09 +1000

    добавлен модуль генерации

---
+++ generate_study_materials/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GenerateStudyMaterialsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'generate_study_materials'
